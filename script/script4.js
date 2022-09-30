6e76a60fcbc44eecb7ef292ad14283d128966b81
Markov-Andrey <andremarkov@icloud.com>
2022-09-30 23:46:24 +0300

    task 1 complete

--- script/script4.js
+++ script/script4.js
@@ -1,4 +1,12 @@
 //Task 1
-function task1Array () {
-
+function task1Array() {
+	document.write (`Выводим значения массива через цикл 
+	от 0 до последнего значения, вычиленного методом ".length" массива<br>`)
+	let task1Array = [1,2,3,4,5],
+	arrayLength = task1Array.length;
+	for (let i = 0; i < arrayLength; i++) {
+		document.write (`${task1Array[i]} `)
+	};
 };
+
+//Task 2
